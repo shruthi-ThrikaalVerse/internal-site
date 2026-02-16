7105ec2a3c6c0f703eba75d14c0cb68b90ff46ef
shruthi-ThrikaalVerse <nandhigamashruthi@gmial.com>
2026-02-16 12:08:53 +0530

    added support domain email

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,13 @@ declare module '*.svg' {
   const content: any;
   export default content;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_EMAILJS_PUBLIC_KEY: string;
+  readonly VITE_EMAILJS_SERVICE_ID: string;
+  readonly VITE_EMAILJS_TEMPLATE_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
